6fbdec0725e3790a7addc7c547581a2dbeff9b7a
David Catling <davecatling@gmail.com>
2022-09-17 17:41:35 +1000

    Add project files.

---
+++ BirthdayAttackDemo/BirthdayAttacker.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BirthdayAttackDemo
+{
+    public class BirthdayAttacker
+    {
+        private SHA256 _sha256 = SHA256.Create();
+        private List<string> _targetFileContents = new List<string>();
+        private Random _random = new Random();
+
+        public int MaxEndLineSpaces { get; private set; }
+        public string RealPath { get; private set; }
+        public string FakePath { get; private set; }
+        public string RealHash { get; private set; }
+        public string FakeHash { get; private set; }
+        public int HashEndCharsInCommon { get; private set; }
+
+        public BirthdayAttacker(string realPath, string fakePath, int maxEndLineSpaces)
+        {
+            RealPath = realPath;
+            FakePath = fakePath;
+            MaxEndLineSpaces = maxEndLineSpaces;
+            RealHash = Sha256HashOfFile(realPath);
+        }
+
+        public string Sha256HashOfFile(string path)
+        {            
+            using var stream = File.OpenRead(path);
+            var hash = _sha256.ComputeHash(stream);
+            return ByteArrayToString(hash);
+        }
+
+        private void ReadFakeFile()
+        {
+            var eof = false;
+            _targetFileContents.Clear();
+            using var textReader = new StreamReader(FakePath);
+            string? nextLine;
+            while (!eof)
+            {
+                nextLine = textReader.ReadLine();
+                if (nextLine != null)
+                    _targetFileContents.Add(nextLine);
+                else
+                    eof = true;
+            }
+        }
+
+        public void MakeModifiedFile()
+        {
+            if (_targetFileContents.Count == 0) ReadFakeFile();
+            var regex = new Regex(@"(\s){1,}$");
+            var lineToChange = _random.Next(0, _targetFileContents.Count -1);
+            var textLine = _targetFileContents.ElementAt(lineToChange);
+            var match = regex.Match(textLine);
+            if (match.Success)
+            {
+                if (match.Groups[0].Length >= MaxEndLineSpaces)
+                    textLine = textLine.TrimEnd();
+                else
+                    textLine += ' ';            
+            }
+            else
+             textLine += ' ';
+            _targetFileContents[lineToChange] = textLine;
+            WriteModifiedFile();
+            FakeHash = Sha256HashOfFile(FakePath);
+            HashEndCharsInCommon = EndHashCharsMatch();
+        }
+
+        private int EndHashCharsMatch()
+        {
+            int matchingChars = 0;
+            for (var i = RealHash.Length-1; i>=0; i--)
+            {
+                if (RealHash[i] == FakeHash[i])
+                    matchingChars++;
+                else
+                    break;
+            }
+            return matchingChars;
+        }
+
+        private void WriteModifiedFile()
+        {
+            using var textWriter = new StreamWriter(FakePath);
+            _targetFileContents.ForEach(line => textWriter.WriteLine(line));
+        }
+
+        private static string ByteArrayToString(byte[] array)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in array)
+                sb.Append(c.ToString("X2"));
+            return sb.ToString();
+        }
+    }
+}
---
+++ BirthdayAttackDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace BirthdayAttackDemo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private const string REAL_PATH = @"C:\Confession\confession_real.txt";
+        private const string FAKE_PATH = @"C:\Confession\confession_fake.txt";
+
+        static void Main(string[] args)
+        {
+            var birthdayAttacker = new BirthdayAttacker(REAL_PATH,FAKE_PATH,2);
+            Console.WriteLine($"Real SHA256: {birthdayAttacker.RealHash}");
+            var attempts = 0;
+            while (birthdayAttacker.HashEndCharsInCommon < 4)
+            {
+                birthdayAttacker.MakeModifiedFile();
+                attempts++;
+                Console.WriteLine($"Real SHA256: { birthdayAttacker.RealHash}");
+                Console.WriteLine($"Fake SHA256: {birthdayAttacker.FakeHash}");
+                Console.WriteLine($"In common: {birthdayAttacker.HashEndCharsInCommon} Attempts: {attempts}");
+                Console.WriteLine("----------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
